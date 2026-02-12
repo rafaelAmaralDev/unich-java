c6ac91309a396a8dc60d96b0cb46ce0c75ae8853
Rafael Amaral <rafaglhf@gmail.com>
2026-02-11 21:32:07 -0300

    first commit

---
+++ src/ExemploSaidaDados.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ExemploSaidaDados {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nome;
+        int idade;
+
+        System.out.print("Digite seu nome:");
+        nome = input.nextLine();
+        
+        System.out.print("Digite sua idade:");
+        idade = input.nextInt();
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Olá, eu sou Java! Seu nome é " + nome + " e tem " + idade + " anos!");
+    }
+}
