e750a5806c71f92f4729793e1102d77619f4b9a0
Rafael Amaral <rafaglhf@gmail.com>
2026-02-23 21:09:12 -0300

    tarefa de sala olerite

---
+++ src/Olerite.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Olerite {
+    public static void main(String[] args) {
+        double valorHora;
+        int quantidadeHora;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor da hora trabalhada:");
+        valorHora = input.nextDouble();
+
+        System.out.print("Digite a quantidade de horas trabalhadas:");
+        quantidadeHora = input.nextInt();
+
+        double salarioBruto;
+        salarioBruto = valorHora * quantidadeHora;
+        double inss = 0.14 * salarioBruto;
+        double ir = 0.15 * salarioBruto;
+        double sindicato  = 0.05 * salarioBruto;
+        double salarioLiquido = salarioBruto - (inss + ir + sindicato);
+
+        System.out.println("+ Salário Bruto: " + salarioBruto);
+        System.out.println("- INSS: " + inss);
+        System.out.println("- IR: " + ir);
+        System.out.println("- Sindicato: " + sindicato);
+        System.out.println("Salário Líquido: R$ " + salarioLiquido);
+    }
+}
