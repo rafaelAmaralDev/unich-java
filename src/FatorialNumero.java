c014ced45514fdea71bb0803085a7c8f6f191339
Rafael Amaral <rafaglhf@gmail.com>
2026-03-02 21:35:30 -0300

    Adicionando classe FatorialNumero e configuração no launch.json

---
+++ src/FatorialNumero.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FatorialNumero {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int valor;
+
+        System.out.println("Número: ");
+        valor = input.nextInt();
+        if (valor < 0) {
+            System.out.println("Valor inválido");
+        } else {
+            int fatorial = 1;
+            for(int i = 1; i <= valor; i++) {
+                fatorial =  fatorial * i;
+            }
+            System.out.println("Fatorial: " + fatorial);
+        }
+    }
+}
