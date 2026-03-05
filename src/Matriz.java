6f9082dab6c09970d53d03238e2701d3130ff329
Rafael Amaral <rafaglhf@gmail.com>
2026-03-04 21:26:57 -0300

    Atualizando a classe Matriz para permitir a leitura de matrizes de qualquer tamanho e calcular a soma da diagonal principal

--- src/Matriz.java
+++ src/Matriz.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
 public class Matriz {
-    //faça um programa que leia uma matriz de 3x3 elementos. calcule a soma dos elementos que estão na diagonal principal.
+    //faça um programa que leia uma matriz de qualquer quantidade de  elementos mas precisa ser o mesmo número, se não for, precisa dizer que não pode efetuar operação. calcule a soma dos elementos que estão na diagonal principal.
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[][] matriz = new int[3][3];
+        System.out.print("Digite o número de linhas da matriz: ");
+        int linhas = input.nextInt();
+        System.out.print("Digite o número de colunas da matriz: ");
+        int colunas = input.nextInt();
+        int[][] matriz = new int[linhas][colunas];
         int cotadorDiagonal = 0;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.printf("matriz [%d][%d] = ", i, j);
-                matriz[i][j] = input.nextInt();
-                if (i == j) {
-                    cotadorDiagonal += matriz[i][j];
+        if (linhas != colunas) {
+            System.out.println("Não é possível efetuar a operação. O número de linhas e colunas deve ser o mesmo.");
+        } else {
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    System.out.printf("matriz [%d][%d] = ", i, j);
+                    matriz[i][j] = input.nextInt();
+                    if (i == j) {
+                        cotadorDiagonal += matriz[i][j];
+                    }
                 }
             }
+            System.out.println("Soma dos elementos da diagonal principal: " + cotadorDiagonal);
         }
-        System.out.println("Soma dos elementos da diagonal principal: " + cotadorDiagonal);
     }
 }
